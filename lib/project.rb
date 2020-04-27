71938fedadf15da3aea222ca05e6887d4e121f65
Trevor Low <tlow94@uw.edu>
2020-04-27 21:42:23 +0000

    Automatically backed up by Learn

--- lib/project.rb
+++ lib/project.rb
@@ -6,4 +6,11 @@ class Project
   def add_backer(backer)
     ProjectBacker.new(self, backer)
   end
+  def find_backers
+    ProjectBacker.all.select {|backer| backer.project == self}
+
+  end
+  def backers
+    find_backers.map {|backer| backer.backer}
+  end
 end
