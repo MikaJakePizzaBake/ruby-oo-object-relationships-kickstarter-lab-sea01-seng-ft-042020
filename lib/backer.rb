e8f98f0204eabaf742cf11ab29e0a509907ef1e3
Trevor Low <tlow94@uw.edu>
2020-04-27 21:29:10 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,9 +1,11 @@
+require 'pry'
 class Backer
   attr_reader :name
   def initialize (name)
     @name = name
   end
   def back_project(project)
+    binding.pry
     ProjectBacker.all.select {|project| project.backer == self}
   end
 end
